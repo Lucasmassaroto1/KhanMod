399cb15b67cb164b7627eae99609651146b2abc5
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-04-21 01:09:41 -0300

    Url

--- Khanware.js
+++ Khanware.js
@@ -1,7 +1,7 @@
 const ver = "V1.0";
 let isDev = false;
 
-const repoPath = `https://raw.githubusercontent.com/Lucasmassaroto1/KhanMod/refs/heads/${isDev ? "dev/" : "main/"}`;
+const repoPath = `https://raw.githubusercontent.com/Lucasmassaroto1/KhanMod/main/`;
 
 let device ={
     mobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone|Mobile|Tablet|Kindle|Silk|PlayBook|BB10/i.test(navigator.userAgent),
